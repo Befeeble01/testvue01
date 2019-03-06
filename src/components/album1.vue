f52c457506ccc4751fc4c496d9dfb6e8dd837c56
pisitpong <40911581+Befeeble01@users.noreply.github.com>
2019-03-06 11:17:14 +0700

    Add files via upload

---
+++ src/components/album1.vue
@@ -0,0 +1,62 @@
+<template>  
+  <div id="app">
+  {{ posts }}
+</div>
+</template>
+
+<script>   
+var dataURL = 'https://jsonplaceholder.typicode.com/posts';
+
+var App = new Vue({
+  el: '#app',
+  data: {
+    posts: [] // initialize empty array
+  },
+  mounted() { // when the Vue app is booted up, this is run automatically.
+    var self = this // create a closure to access component in the callback below
+    $.getJSON(dataURL, function(data) {
+      self.posts = data;
+    });
+  }
+})
+</script>
+
+<style>  
+.test-list {
+  font-family: Roboto;
+  list-style: none;
+  margin: 20px auto;
+  width: 50%;
+}
+
+.test-list--item {
+  border: 1px solid rgb(41, 41, 41);
+  border-radius: 5px;
+  text-align: center;
+  display: block;
+  box-shadow: 2px 2px rgba(138, 124, 124, 0.4);
+}
+
+.test-list--id {
+  font-weight: 300;
+  margin: 10px auto;
+}
+
+.test-list--title {
+  font-weight: 500;
+  margin: 20px auto;
+  text-transform: capitalize;
+}
+
+.test-list--complete {
+  font-weight: 600;
+  margin: 10px auto;
+  color: #56ca86;
+}
+
+.test-list--incomplete {
+  font-weight: 600;
+  margin: 10px auto;
+  color: #ca5656;
+}
+</style>  
---
+++ src/views/photo1.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="photo1">
+    <album1/>
+  </div>
+</template>
+
+<script>
+import { Component, Vue } from 'vue-property-decorator';
+import album1    from '@/components/album1.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    album1,
+  },
+})
+export default class photo1 extends Vue {}
+</script>
+
+<style>
+
+</style>
